c726de18ce1330b67fea108baa5f95db2718a660
Vinod Kotagiri <vinodkotagiri@icloud.com>
2022-10-14 18:06:46 +0530

    added home nav bar

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './component.header'
 export { default as BackgroundImage } from './component.background-image'
 export { default as Loader } from './component.loader'
+export { default as Navbar } from './component.navbar'
--- src/pages/page.home.jsx
+++ src/pages/page.home.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Navbar } from '../components'
 const HomePage = () => {
-	return <div>HomePage</div>
+	const [isScrolled, setIsScrolled] = useState(false)
+	window.onscroll = () => {
+		setIsScrolled(window.pageYOffset === 0 ? false : true)
+		return () => (window.onscroll = null)
+	}
+
+	return (
+		<div>
+			<Navbar isScrolled={isScrolled} />
+		</div>
+	)
 }
 
 export default HomePage
