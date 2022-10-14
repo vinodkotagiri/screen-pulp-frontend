7a7bb6a2318b39dec442d1a91d43eef853d91f49
Vinod Kotagiri <vinodkotagiri@icloud.com>
2022-10-14 23:37:04 +0530

    removed context"

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
-import { MovieProvider } from './context/movies'
 import axios from 'axios'
 import { TMDB_BASE_URL } from './utils/api'
 axios.defaults.baseURL = TMDB_BASE_URL
 const root = createRoot(document.getElementById('root'))
 root.render(
-	<MovieProvider>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</MovieProvider>
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
 )
